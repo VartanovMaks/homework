2352e3c6383e8c0e0445be72fe1982e8080287eb
VartanovMaks <vartanov2009@gmail.com>
2021-06-13 19:40:38 +0300

    example from tutorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,19 @@
-import { HomeComponent } from '../app/components/app/components/home/home.component';
 
-import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './components/app/app.component';
+import { SizerComponent } from './components/sizer/sizer.component';
 
-let routes:Routes = [
-  {path:'users', loadChildren:()=>import('./components/modules/user/user.module').then(m=>m.UserModule)},
-  {path:'posts', loadChildren:()=>import('./components/modules/post/post.module').then(m=>m.PostModule)},
-  {path:'**', component:HomeComponent},
-];
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    SizerComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
